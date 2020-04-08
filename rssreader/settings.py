1cca71e2bc71b4eb6aeef86294098996d9f9765f
Tomas Balaz <tomasbalaz3012@gmail.com>
2020-04-08 02:28:38 +0200

    allowed host to azure only and localhost -complete azure address

--- rssreader/settings.py
+++ rssreader/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '-hb^s88-$*^n&e@znnj3dz1mui*u66e)fj3v(yc046_s^lumgv'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['www.django-rss.scm.azurewebsites.net/']
+ALLOWED_HOSTS = ['https://django-rss.azurewebsites.net/', '127.0.0.1:8000']
 
 
 # Application definition
