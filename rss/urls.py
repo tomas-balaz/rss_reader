03d2682dc068ea52352942dbf25a9e7ee9c8533a
Tomas Balaz <tomasbalaz3012@gmail.com>
2020-04-08 11:33:01 +0200

    rooting

--- rss/urls.py
+++ rss/urls.py
@@ -1,11 +1,17 @@
-from django.urls import path, include
+from django.urls import path, include, re_path
 from . import views
+from django.views.static import serve 
+from django.conf import settings
+
 
 
 urlpatterns= [
 
     path('', views.index, name='index'),
     path('sortA/', views.sort_articles_ascending, name='ascending'),
-    path('sortD/', views.sort_articles_descending, name='descending')
+    path('sortD/', views.sort_articles_descending, name='descending'),
+
+    re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 
 ]
